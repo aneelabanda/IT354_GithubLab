30faf6cb332506ffeb8fb1a09f2387a279d6c487
Anusha Vemula <vemulaanusha59@gmail.com>
2024-02-14 11:40:33 -0600

    declaringconst_js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const board =[]
+
+const board = [
+[0, 0, 0, 0],
+[0, 0, 0, 0],
+[0, 0, 0, 0],
+[0, 0, 0, 0]
+]
+//const board=[
+//[0, 2, 4, 8],
+//[16, 32, 64, 1281],
+//[256, 512, 1024, 0],
+//[0, 0, 0, 0]
+// ]
+const boardContainer = document.querySelector(".board");
+document.addEventListener ('swiped-up', function(e) {
+console. log(e.target); // the element that was swiped
+});
